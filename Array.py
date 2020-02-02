5103b289376318f199eae6744c367b89660c46c0
akashlahane33 <60478648+akashlahane33@users.noreply.github.com>
2020-02-02 11:22:46 +0530

    commiting files

---
+++ Array.py
@@ -0,0 +1,16 @@
+#import array as arr  - we can import like this also..but we will import like below
+from numpy import *
+from array import *
+
+vals = array('i',[5,9,8,4,2])
+
+print(vals)
+print(vals.buffer_info())  #array functions
+vals.reverse()
+print(vals)
+
+print(vals[1])
+
+for i in range(5):
+    print(vals[i])
+
